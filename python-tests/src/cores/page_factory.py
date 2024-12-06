eb93ec6c25de44bc4b927e2c81b6d6f6e1bc5f52
Trần Văn <32728582+tranvan1202@users.noreply.github.com>
2024-12-06 14:36:25 +0700

    update 0.9.11: update code

---
+++ python-tests/src/cores/page_factory.py
@@ -0,0 +1,34 @@
+from src.pages.i_hybris_promotion_rules_page import IHybrisPromotionRulesPage
+from src.pages.i_pdpage import IQPDPage
+from src.pages.s_normal_pdpage import SSPDPage
+
+class PageFactory:
+    """
+    Factory class to create Page Object instances based on domain and page type.
+    """
+    PAGE_MAPPING = {
+        "iq": {
+            "pdp": IQPDPage,
+            "hybris": IHybrisPromotionRulesPage
+        },
+        "ss": {
+            "normal_pdp": SSPDPage
+        },
+        # Add mappings for other domains and page types
+    }
+
+    @staticmethod
+    def create_page(domain: str, page_type: str, page, device: str):
+        # Check if the domain exists in the mapping
+        if domain not in PageFactory.PAGE_MAPPING:
+            raise ValueError(f"Unsupported domain: {domain}")
+
+        # Check if the page type exists for the domain
+        if page_type not in PageFactory.PAGE_MAPPING[domain]:
+            raise ValueError(f"Unsupported page type '{page_type}' for domain '{domain}'")
+
+        # Get the appropriate Page Object class
+        page_class = PageFactory.PAGE_MAPPING[domain][page_type]
+
+        # Return an instance of the Page Object class
+        return page_class(page, device)
---
+++ python-tests/src/pages/i_hybris_promotion_rules_page.py
@@ -0,0 +1,27 @@
+# python-tests/src/pages/i_hybris_promotion_rules_page.py
+import time
+from src.pages.base_page import BasePage
+
+class IHybrisPromotionRulesPage(BasePage):
+    def __init__(self, page, device):
+        super().__init__(page, device)
+        self.ih_marketing_tab_name = page.locator("td[class='y-tree-icon-hmc_treenode_marketing z-treecell'] span[class='z-label']")
+        self.ih_marketing_promotion_rules_tab_name = page.locator("td[class='y-tree-icon-hmc_typenode_promotion_rules z-treecell'] span[class='z-label']")
+        self.ih_marketing_promotion_rules_search = page.locator("input[placeholder='Type to search']")
+
+    def trigger_lazy_load_actions(self, page=None):
+        """
+        Override trigger_load_actions to define IQPD-specific actions.
+        :param page: Optional specific page object (defaults to self.page).
+        """
+        page = page or self.page
+
+        #Trigger lazy-load elements
+        page.wait_for_load_state("domcontentloaded")
+        self.actions.wait_and_click_element(self.ih_marketing_tab_name, page=page)
+        time.sleep(0.5)
+        self.actions.wait_and_click_element(self.ih_marketing_promotion_rules_tab_name, page=page)
+        time.sleep(0.5)
+        self.actions.wait_and_click_element(self.ih_marketing_promotion_rules_search, page=page)
+        self.ih_marketing_promotion_rules_search.fill("test")
+        time.sleep(5.5)
